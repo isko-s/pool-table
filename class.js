30e32239d9db15d74dcbbe2f2b3806bf1becd9f1
Ismail <Ismail@DESKTOP-G0GSDUM>
2022-03-10 15:33:25 +0100

    init

---
+++ class.js
@@ -0,0 +1,7 @@
+function ispis(){
+    let paragraf = document.querySelector(".relative");
+    paragraf.innerHTML="Good Luck :-(";
+    paragraf.style.color="#fdc503";
+    paragraf.style.fontSize="100px";
+    paragraf.style.textAlign="center";
+} 
